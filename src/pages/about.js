4becad538eac01ca12c3f8ee4349ce60ce4e8158
Tom Waterton <tomerton@uk.ibm.com>
2020-01-04 07:21:00 +0000

    Home page, About, and update styles

--- src/pages/about.js
+++ src/pages/about.js
@@ -3,7 +3,7 @@ import React from "react";
 import Layout from "../components/layout";
 import SEO from "../components/seo";
 import AboutIllustration from "../images/about/about.jpg";
-
+import TypewriterIllustration from "../images/favicon/typewriter.svg";
 
 function AboutPage() {
   return (
@@ -13,32 +13,19 @@ function AboutPage() {
         title="About"
       />
 
-<section class="row">
+  <section class="row">
     <div class="column">
       <h1>About this site</h1>
-      <p class="description padding-bottom">I built this personal website as a little side project.</p>
-      <p>Technical details:</p>
-      <ul class="padding-bottom">
-        <li>I built this site using the free and open source&nbsp;
-            <a href="https://www.gatsbyjs.org/" target="_blank">Gatsby</a>
-            &nbsp;React framework.
-        </li>
-        <li>To get started, I used the&nbsp;
-            <a href="https://github.com/taylorbryant/gatsby-starter-tailwind" target="_blank">Tailwind</a>
-            &nbsp;Gatsby starter theme, which I then adapted to suit my needs.
-        </li>
-        <li>I use&nbsp;
-            <a href="https://github.com/" target="_blank">GitHub</a>
-            &nbsp;as my source repository.
-        </li>
-        <li>And I use&nbsp;
-            <a href="https://pages.github.com/" target="_blank">GitHub Pages</a>
-            &nbsp;to host the site.
-        </li>
-      </ul>
-      <p class="description">If you spot any bugs, do&nbsp;
-          <a href="mailto:tomwaterton@gmail.com?Subject=Re%20your%20website">get in touch</a> with me.
-      </p>
+      <p class="description">I'm no web developer, but I do like to learn new things, so 
+      I recently set myself the challenge of creating my own website.</p>
+      <p class="description padding-bottom-small">Here you'll find some of the random things that float through 
+      my mind and which make up my life.</p>
+      <p class="description padding-bottom-small">Tom</p>
+      <img
+        alt="Typewriter icon"
+        className="block site-icon padding-bottom"
+        src={TypewriterIllustration}
+      />
     </div>
     <div class="column">
       <img
@@ -46,6 +33,20 @@ function AboutPage() {
         className="block tilt-landscape"
         src={AboutIllustration}
       />
+    </div>
+  </section>
+
+  <section class="row">
+    <div class="column">
+      <h2>Technical details</h2>
+      <ul class="padding-bottom">
+        <li>I built this site using the free and open source <a href="https://www.gatsbyjs.org/" target="_blank">Gatsby</a> React framework.</li>
+        <li>To get started, I used the <a href="https://github.com/taylorbryant/gatsby-starter-tailwind" target="_blank">Tailwind</a> Gatsby starter theme, which I then adapted to suit my needs.</li>
+        <li>I use <a href="https://github.com/" target="_blank">GitHub</a> as my source repository.</li>
+        <li>And I use <a href="https://pages.github.com/" target="_blank">GitHub Pages</a> to host the site.</li>
+      </ul>
+      <p class="description">If you spot any bugs, please do <a href="mailto:tomwaterton@gmail.com?Subject=Re%20your%20website">get in touch</a> with me.
+      </p>
     </div>
   </section>
 
