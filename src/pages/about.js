0228f2ae35acf005f99150f8f037dba451d4c332
Tom Waterton <tomerton@uk.ibm.com>
2022-06-14 05:53:23 +0100

    Test change

--- src/pages/about.js
+++ src/pages/about.js
@@ -16,7 +16,7 @@ function AboutPage() {
   <section className="row">
     <div className="column">
       <h1>About this site</h1>
-      <p className="description">I'm not really a web developer, but I do like to learn new things, so 
+      <p className="description">I'm not a proper web developer, but I do like to learn new things, so 
       at the beginning of 2020 I set myself the challenge of creating my own website.</p>
       <p className="description padding-bottom-small">Here you'll find some information about my work as 
       a content designer, as well as some of my thoughts, hobbies, and interests. Enjoy!</p>
