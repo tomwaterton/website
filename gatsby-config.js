70541fa22fe7a1cfacb74f57dd09526428aca353
tomwaterton <tomwaterton@gmail.com>
2022-10-21 06:36:29 +0100

    GA config

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,18 +10,11 @@ module.exports = {
   pathPrefix: "/",
   plugins: [
     {
-      resolve: 'gatsby-plugin-manifest',
-      options: {
-        icon: 'src/images/favicon/typewriter.svg',
-      },
-    },
-    {
       resolve: `gatsby-plugin-google-gtag`,
       options: {
         // You can add multiple tracking ids and a pageview event will be fired for all of them.
         trackingIds: [
-          "G-LTGBY7DN1E", // Added 21 Sep 2022 (stream ID 3272363831)
-          "G-FLF83ZC9XP" // Added 21 Sep 2022 (2411352490)
+          "G-FLF83ZC9XP", // Added 21 Sep 2022 (2411352490)
         ],
         // This object gets passed directly to the gtag config command
         // This config will be shared across all trackingIds
@@ -39,6 +32,12 @@ module.exports = {
           // Avoids sending pageview hits from custom paths
           exclude: ["/preview/**", "/do-not-track/me/too/"],
         },
+      },
+    },
+    {
+      resolve: 'gatsby-plugin-manifest',
+      options: {
+        icon: 'src/images/favicon/typewriter.svg',
       },
     },
     'gatsby-plugin-cname',
