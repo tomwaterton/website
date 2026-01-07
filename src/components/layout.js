ba8d215c3d4266306649587c5bb122a97c794070
Tom Waterton <tomwaterton@gmail.com>
2026-01-07 16:23:35 +0000

    Updates books and deps

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,4 +1,4 @@
-import { Link } from 'gatsby';
+import { Link } from "gatsby";
 import PropTypes from "prop-types";
 import React from "react";
 
@@ -12,44 +12,47 @@ function Layout(props) {
     <div className="pageWrapper">
       <Header />
 
-      <main>
-        {props.children}
-      </main>
+      <main>{props.children}</main>
 
       <footer className="footer do-not-print">
         <div className="footerContainer">
-            <div className="footerContainer2">
-              <div className="footerContainer3">
-                <div className="footerCopyright">
-                    &copy; Tom Waterton, 2025
-                </div>
-                <div className="footerInfo">
-                  <Link to="/about">About this site</Link>
-                </div>
+          <div className="footerContainer2">
+            <div className="footerContainer3">
+              <div className="footerCopyright">
+                &copy; Tom Waterton, 2020–2026
               </div>
-              <div className="socialMediaContainer">
-                <div className="socialMedia">
-                  <ul>
-                    <li>
-                      <a href="https://www.linkedin.com/in/tomwaterton/">
-                        <img alt="LinkedIn icon" src={linkedin} />
-                      </a>
-                    </li>
-                    <li>
-                      <a href="https://twitter.com/tomwaterton">
-                        <img alt="Twitter icon" src={twitter} />
-                      </a>
-                    </li>
-                    <li>
-                      <a href="mailto:tomwaterton@gmail.com">
-                        <img alt="Email icon" src={email} width="32px" height="32px"/>
-                      </a>
-                    </li>
-                  </ul>
-                </div>
+              <div className="footerInfo">
+                <Link to="/about">About this site</Link>
+              </div>
+            </div>
+            <div className="socialMediaContainer">
+              <div className="socialMedia">
+                <ul>
+                  <li>
+                    <a href="https://www.linkedin.com/in/tomwaterton/">
+                      <img alt="LinkedIn icon" src={linkedin} />
+                    </a>
+                  </li>
+                  <li>
+                    <a href="https://twitter.com/tomwaterton">
+                      <img alt="Twitter icon" src={twitter} />
+                    </a>
+                  </li>
+                  <li>
+                    <a href="mailto:tomwaterton@gmail.com">
+                      <img
+                        alt="Email icon"
+                        src={email}
+                        width="32px"
+                        height="32px"
+                      />
+                    </a>
+                  </li>
+                </ul>
               </div>
             </div>
           </div>
+        </div>
       </footer>
     </div>
   );
